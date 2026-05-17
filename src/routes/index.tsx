577c4d5507114aeac02e50dd63c0e2207a4e6de0
Dev <dev@workspace.local>
2026-05-17 18:54:08 +0100

    new b

--- src/routes/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { TicketLookup } from "@/components/ticket-lookup";
-
-export const Route = createFileRoute("/")({
-  component: TicketLookup,
-});
